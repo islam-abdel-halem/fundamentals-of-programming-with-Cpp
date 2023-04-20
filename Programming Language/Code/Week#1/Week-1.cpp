69563e87537b720f08a041d84efd615d3b937c89
ESLAM <102685868+ESLAM80@users.noreply.github.com>
2023-04-20 03:07:53 +0200

    add code

--- Programming Language/Code/Week#1/Week-1.cpp
+++ Programming Language/Code/Week#1/Week-1.cpp
@@ -1,8 +1,31 @@
+// Include Iostream
 #include <iostream>
 
+/* Elzero */
+
+// This Is Main Function
 int main()
 {
-    std::cout << "Iam Here";
+    std::cout << "Line 1\n"; // Comment
+    std::cout /* <= This Is Character Out */ << "Line 2\n";
+    std::cout << "Line 3\n";
+    // Comment
+    std::cout << "std::cout";
     return 0;
-    
-}
+} // Comment
+
+// Search Google For "Iostream"
+
+// ========== Elzero ========== //
+
+//////////// Elzero ////////////
+
+/*
+  ==================
+    Elzero
+  ==================
+*/
+
+/*==================
+    Elzero
+  ==================*/
